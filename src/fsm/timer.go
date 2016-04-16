e55689f41acca6732e5bf168ce0497b309af459c
erlengra <Erlend_grande91@hotmail.com>
2016-04-16 16:01:25 +0200

    Update timer.go

--- src/fsm/timer.go
+++ src/fsm/timer.go
@@ -4,18 +4,20 @@ import (
 	"time"
 )
 
-func doorTimer(timeout chan<- bool, reset <-chan bool) {
-	const doorOpenTime = 3 * time.Second
+func Timer(timeout chan<- bool, reset <-chan bool, get_time <- chan bool) {
+	const duration = 3 * time.Second
 	timer := time.NewTimer(0)
 	timer.Stop()
-
 	for {
 		select {
-		case <-reset:
-			timer.Reset(doorOpenTime)
+		case timer.Now():
+			log.Println(timer.Now())
 		case <-timer.C:
 			timer.Stop()
 			timeout <- true
+		case <-reset:
+			timer.Reset(duration)
+		
 		}
 	}
 }
