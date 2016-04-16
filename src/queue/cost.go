a7f66922a4b518757965eb972645e6a0e46748f4
erlengra <Erlend_grande91@hotmail.com>
2016-04-16 16:05:45 +0200

    Update cost.go

--- src/queue/cost.go
+++ src/queue/cost.go
@@ -1,31 +1,46 @@
 package queue
 
 import (
-	def "config"
+	"../config"
 	"log"
 )
 
 func CalculateCost(targetFloor, targetButton, prevFloor, currFloor, currDir int) int {
 	q := local.deepCopy()
-	q.setOrder(targetFloor, def.BtnInside, Status{true, "", nil})
+
+	// q := new(queue)
+	// for f := 0; f < config.N_Floors; f++ {
+	// 	for b := 0; b < config.N_Buttons; b++ {
+	// 		q.matrix[f][b] = local.matrix[f][b]
+	// 	}
+	// }
+
+
+
+
+	q.setOrder(targetFloor, config.BtnInside, orderStatus{true, "", nil})
 
 	cost := 0
 	floor := prevFloor
 	dir := currDir
 
 	if currFloor == -1 {
+		// Between floors, add 1 cost.
 		cost++
-	} else if dir != def.DirStop {
+	} else if dir != config.DirStop {
+		// At floor, but moving, add 2 cost.
 		cost += 2
 	}
 	floor, dir = incrementFloor(floor, dir)
 
-	for n := 0; !(floor == targetFloor && q.shouldStop(floor, dir)) && n < 10; n++ {
-		if q.shouldStop(floor, dir) {
+	// Simulate how the lift will move, and accumulate cost until it 'reaches' target.
+	// Break after 10 iterations to assure against a stuck loop.
+	for n := 0; !(floor == targetFloor && q.StopElevator(floor, dir)) && n < 10; n++ {
+		if q.StopElevator(floor, dir) {
 			cost += 2
-			q.setOrder(floor, def.BtnUp, inactive)
-			q.setOrder(floor, def.BtnDown, inactive)
-			q.setOrder(floor, def.BtnInside, inactive)
+			q.setOrder(floor, config.BtnUp, inactive)
+			q.setOrder(floor, config.BtnDown, inactive)
+			q.setOrder(floor, config.BtnInside, inactive)
 		}
 		dir = q.chooseDirection(floor, dir)
 		floor, dir = incrementFloor(floor, dir)
@@ -36,25 +51,23 @@ func CalculateCost(targetFloor, targetButton, prevFloor, currFloor, currDir int)
 
 func incrementFloor(floor, dir int) (int, int) {
 	switch dir {
-	case def.DirDown:
+	case config.DirDown:
 		floor--
-	case def.DirUp:
+	case config.DirUp:
 		floor++
-	case def.DirStop:
-		// Don't increment.
+	case config.DirStop:
 	default:
-		def.CloseConnectionChan <- true
-		def.Restart.Run()
-		log.Fatalln(def.ColR, "incrementFloor(): invalid direction, not incremented", def.ColN)
+		config.CloseConnectionChan <- true
+		config.Restart.Run()
 	}
 
-	if floor <= 0 && dir == def.DirDown {
-		dir = def.DirUp
+	if floor <= 0 && dir == config.DirDown {
+		dir = config.DirUp
 		floor = 0
 	}
-	if floor >= def.N_Floors-1 && dir == def.DirUp {
-		dir = def.DirDown
-		floor = def.N_Floors - 1
+	if floor >= config.N_Floors-1 && dir == config.DirUp {
+		dir = config.DirDown
+		floor = config.N_Floors - 1
 	}
 	return floor, dir
 }
