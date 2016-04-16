5348c19f41e131fc5a453f9cd95191248eec9326
erlengra <Erlend_grande91@hotmail.com>
2016-04-16 16:02:16 +0200

    Update lift.go

--- src/hw/lift.go
+++ src/hw/lift.go
@@ -1,51 +1,62 @@
+// Package hw defines interactions with the lift hardware at the real time
+// lab at The Department of Engineering Cybernetics at NTNU, Trondheim,
+// Norway.
+//
+// This file is a golang port of elev.c from the hand out driver
+// (https://github.com/TTK4145/Project)
 package hw
 
 import (
-	def "config"
+	"../config"
 	"errors"
 	"log"
 )
 
-var lampChannelMatrix = [def.N_Floors][def.N_Buttons]int{
+var lampChannelMatrix = [config.N_Floors][config.N_Buttons]int{
 	{LIGHT_UP1, LIGHT_DOWN1, LIGHT_COMMAND1},
 	{LIGHT_UP2, LIGHT_DOWN2, LIGHT_COMMAND2},
 	{LIGHT_UP3, LIGHT_DOWN3, LIGHT_COMMAND3},
 	{LIGHT_UP4, LIGHT_DOWN4, LIGHT_COMMAND4},
 }
-var buttonChannelMatrix = [def.N_Floors][def.N_Buttons]int{
+var buttonChannelMatrix = [config.N_Floors][config.N_Buttons]int{
 	{BUTTON_UP1, BUTTON_DOWN1, BUTTON_COMMAND1},
 	{BUTTON_UP2, BUTTON_DOWN2, BUTTON_COMMAND2},
 	{BUTTON_UP3, BUTTON_DOWN3, BUTTON_COMMAND3},
 	{BUTTON_UP4, BUTTON_DOWN4, BUTTON_COMMAND4},
 }
 
+// Init initialises the lift hardware and moves the lift to a defined state.
+// (Descending until it reaches a floor.)
 func Init() (int, error) {
+	// Init hardware
 	if !ioInit() {
 		return -1, errors.New("Hardware driver: ioInit() failed!")
 	}
 
-	for f := 0; f < def.N_Floors; f++ {
+	// Zero all floor button lamps
+	for f := 0; f < config.N_Floors; f++ {
 		if f != 0 {
-			SetButtonLamp(f, def.BtnDown, false)
+			SetButtonLamp(f, config.BtnDown, false)
 		}
-		if f != def.N_Floors-1 {
-			SetButtonLamp(f, def.BtnUp, false)
+		if f != config.N_Floors-1 {
+			SetButtonLamp(f, config.BtnUp, false)
 		}
-		SetButtonLamp(f, def.BtnInside, false)
+		SetButtonLamp(f, config.BtnInside, false)
 	}
 
 	SetStopLamp(false)
 	SetDoorLamp(false)
 
-	SetMotorDir(def.DirDown)
+	// Move to defined state
+	SetMotorDir(config.DirDown)
 	floor := Floor()
 	for floor == -1 {
 		floor = Floor()
 	}
-	SetMotorDir(def.DirStop)
+	SetMotorDir(config.DirStop)
 	SetFloorLamp(floor)
 
-	log.Println(def.ColG, "Hardware initialised.", def.ColN)
+	log.Println(config.ColG, "Hardware initialised.", config.ColN)
 	return floor, nil
 }
 
@@ -84,12 +95,13 @@ func Floor() int {
 }
 
 func SetFloorLamp(floor int) {
-	if floor < 0 || floor >= def.N_Floors {
+	if floor < 0 || floor >= config.N_Floors {
 		log.Printf("Error: Floor %d out of range!\n", floor)
 		log.Println("No floor indicator will be set.")
 		return
 	}
 
+	// Binary encoding. One light must always be on.
 	if floor&0x02 > 0 {
 		ioSetBit(LIGHT_FLOOR_IND1)
 	} else {
@@ -104,19 +116,19 @@ func SetFloorLamp(floor int) {
 }
 
 func ReadButton(floor int, button int) bool {
-	if floor < 0 || floor >= def.N_Floors {
+	if floor < 0 || floor >= config.N_Floors {
 		log.Printf("Error: Floor %d out of range!\n", floor)
 		return false
 	}
-	if button < 0 || button >= def.N_Buttons {
+	if button < 0 || button >= config.N_Buttons {
 		log.Printf("Error: Button %d out of range!\n", button)
 		return false
 	}
-	if button == def.BtnUp && floor == def.N_Floors-1 {
+	if button == config.BtnUp && floor == config.N_Floors-1 {
 		log.Println("Button up from top floor does not exist!")
 		return false
 	}
-	if button == def.BtnDown && floor == 0 {
+	if button == config.BtnDown && floor == 0 {
 		log.Println("Button down from ground floor does not exist!")
 		return false
 	}
@@ -129,21 +141,21 @@ func ReadButton(floor int, button int) bool {
 }
 
 func SetButtonLamp(floor int, button int, value bool) {
-	if floor < 0 || floor >= def.N_Floors {
+	if floor < 0 || floor >= config.N_Floors {
 		log.Printf("Error: Floor %d out of range!\n", floor)
 		return
 	}
-	if button == def.BtnUp && floor == def.N_Floors-1 {
+	if button == config.BtnUp && floor == config.N_Floors-1 {
 		log.Println("Button up from top floor does not exist!")
 		return
 	}
-	if button == def.BtnDown && floor == 0 {
+	if button == config.BtnDown && floor == 0 {
 		log.Println("Button down from ground floor does not exist!")
 		return
 	}
-	if button != def.BtnUp &&
-		button != def.BtnDown &&
-		button != def.BtnInside {
+	if button != config.BtnUp &&
+		button != config.BtnDown &&
+		button != config.BtnInside {
 		log.Printf("Invalid button %d\n", button)
 		return
 	}
